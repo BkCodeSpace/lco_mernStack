f20e10f5a86fc6886aa5af7c08da6163bbd008f9
BhawaniK <kundala.bhawani@gmail.com>
2023-09-01 18:51:52 +0530

    added userSchema Model

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+var userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    maxlength: 32,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    require: true,
+    unique: true,
+  },
+  userinfo:{
+    type:String,
+    trim:true
+  },
+  //TODO : password
+  password: {
+    type: String,
+    trim: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+});
+
+module.exports = mongoose.model('User', userSchema)
